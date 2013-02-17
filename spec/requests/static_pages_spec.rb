f7967def6fb8e10ceeb6c882f8d142bbad596993
Greg Daviet <Greg@theDaviets.com>
2013-02-17 16:45:06 -0700

    Add titles, about page, and contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,20 +1,66 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+
+  let(:base_title) { "iFarmPro" }
   
   describe "Home page" do
-    
-    it "should have the content 'iFarm'" do
+
+    before do
       visit '/static_pages/home'
-      page.should have_content('iFarm')  
+    end
+    
+    it "should have content iFarmPro" do
+      expect(page).to have_selector('h1', :text => 'iFarmPro')
+    end
+
+    it "should have the correct title" do
+      expect(page).to have_selector('title', :text => "#{base_title} | Home")
     end  
   end
 
   describe "Help page" do
     
-    it "should have the content 'Help'" do
+    before do
       visit '/static_pages/help'
-      page.should have_content('Help')
     end
+
+    it "should have the content 'Help'" do
+      expect(page).to have_selector('h1', :text => 'Help')
+    end
+
+    it "should have the correct title" do
+      expect(page).to have_selector('title', :text => "#{base_title} | Help")
+    end  
+  end
+
+  describe "About page" do
+    
+    before do
+      visit '/static_pages/about'
+    end
+
+    it "should have the content 'About'" do
+      expect(page).to have_selector('h1', :text => 'About')
+    end
+
+    it "should have the correct title" do
+      expect(page).to have_selector('title', :text => "#{base_title} | About")
+    end  
+  end
+
+  describe "Contact page" do
+
+    before do
+      visit '/static_pages/contact'
+    end
+
+    it "should have the content 'Contact iFarm" do
+      page.should have_selector("h1", :text => "Contact iFarm")
+    end
+
+    it "should have the correct title" do
+      expect(page).to have_selector('title', :text => "#{base_title} | Contact")
+    end  
   end
 end
