c120d4c490b05fb2dd4ae47267d11e77a7a3b619
Greg Daviet <Greg@theDaviets.com>
2013-02-17 09:33:48 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ifarm::Application.config.secret_token = 'fcd5e116535a462ada1abb67baa89614aa900c4af546c225ba095d24039d598339967397d28251f41ab1af369e99544e091381503e25e4ef6b32ae081e5bb850'
