fa392df637ba8f056e3c5bec5bcc049b859b76dc
Greg Daviet <Greg@theDaviets.com>
2013-02-19 19:34:23 -0700

    Add User#new page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Ifarm::Application.routes.draw do
-root to: 'static_pages#home'
+  get "users/new"
 
-  match '/help', to: 'static_pages#help'
+  root to: 'static_pages#home'
+
+  match '/signup',  to:'users#new'
+  
+  match '/help',    to: 'static_pages#help'
   match '/contact', to: 'static_pages#contact'
-  match '/about', to: 'static_pages#about'
+  match '/about',   to: 'static_pages#about'
   
   # The priority is based upon order of creation:
   # first created -> highest priority.
