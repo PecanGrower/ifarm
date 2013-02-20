b70e769e460dde3dc3d4ed6fa7dd569f279e86e5
Greg Daviet <Greg@theDaviets.com>
2013-02-19 18:42:07 -0700

    Refactor static_pages_spec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,68 +3,39 @@ require 'spec_helper'
 describe "StaticPages" do
 
   let(:base_title) { "iFarmPro" }
+
+  subject { page }
   
   describe "Home page" do
 
-    before do
-      visit root_path
-    end
-    
-    it "should have content iFarmPro" do
-      expect(page).to have_selector('h1', :text => 'iFarmPro')
-    end
+    before { visit root_path }
 
-    it "should have the correct title" do
-      expect(page).to have_selector('title', :text => "#{base_title}")
-    end  
-
-    it "should not have a custom page title" do
-      expect(page).not_to have_selector('title', :text => "| Home")
-    end
+    it { should have_selector('h1', :text => 'iFarmPro') }
+    it { should have_selector('title', :text => "#{base_title}") }
+    it { should_not have_selector('title', :text => "| Home") }
   end
 
   describe "Help page" do
     
-    before do
-      visit help_path
-    end
+    before { visit help_path }
 
-    it "should have the content 'Help'" do
-      expect(page).to have_selector('h1', :text => 'Help')
-    end
-
-    it "should have the correct title" do
-      expect(page).to have_selector('title', :text => "#{base_title} | Help")
-    end  
+    it { should have_selector('h1', :text => 'Help') }
+    it { should have_selector('title', :text => "#{base_title} | Help") }
   end
 
   describe "About page" do
     
-    before do
-      visit about_path
-    end
+    before { visit about_path }
 
-    it "should have the content 'About'" do
-      expect(page).to have_selector('h1', :text => 'About')
-    end
-
-    it "should have the correct title" do
-      expect(page).to have_selector('title', :text => "#{base_title} | About")
-    end  
+    it { should have_selector('h1', :text => 'About') }
+    it { should have_selector('title', :text => "#{base_title} | About") }
   end
 
   describe "Contact page" do
 
-    before do
-      visit contact_path
-    end
+    before { visit contact_path }
 
-    it "should have the content 'Contact iFarm" do
-      page.should have_selector("h1", :text => "Contact iFarm")
-    end
-
-    it "should have the correct title" do
-      expect(page).to have_selector('title', :text => "#{base_title} | Contact")
-    end  
+    it { should have_selector("h1", :text => "Contact iFarm") }
+    it { should have_selector('title', :text => "#{base_title} | Contact") }
   end
 end
