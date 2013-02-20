b9520b729900a524353f2721a96264bf1503150d
Greg Daviet <Greg@theDaviets.com>
2013-02-19 19:45:13 -0700

    Refactor static_pages_spec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,39 +2,49 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
-  let(:base_title) { "iFarmPro" }
+  # let(:base_title) { "iFarmPro" }
 
   subject { page }
+
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_selector('title', text: full_title(page_title)) }
+  end
   
   describe "Home page" do
 
     before { visit root_path }
+    let(:heading) { 'iFarmPro' }
+    let(:page_title) { '' }
 
-    it { should have_selector('h1', :text => 'iFarmPro') }
-    it { should have_selector('title', :text => full_title('')) }
+    it_should_behave_like "all static pages"
+    it { should_not have_selector('title', text: '| Home') }
   end
 
   describe "Help page" do
     
     before { visit help_path }
+    let(:heading) { 'Help' }
+    let(:page_title) { 'Help' }
 
-    it { should have_selector('h1', :text => 'Help') }
-    it { should have_selector('title', :text => full_title('Help')) }
+    it_should_behave_like "all static pages"
   end
 
   describe "About page" do
     
     before { visit about_path }
+    let(:heading) { 'About iFarmPro' }
+    let(:page_title) { 'About' }
 
-    it { should have_selector('h1', :text => 'About') }
-    it { should have_selector('title', :text => full_title('About')) }
+    it_should_behave_like "all static pages"
   end
 
   describe "Contact page" do
 
     before { visit contact_path }
+    let(:heading) { 'Contact iFarmPro' }
+    let(:page_title) { 'Contact' }
 
-    it { should have_selector("h1", :text => "Contact iFarm") }
-    it { should have_selector('title', :text => full_title('Contact')) }
+    it_should_behave_like "all static pages"
   end
 end
