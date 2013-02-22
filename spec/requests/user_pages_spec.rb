56ca8d29e1fdcf7e898b500e699a773fd0a91bf6
Greg Daviet <Greg@theDaviets.com>
2013-02-22 13:29:37 -0700

    Add User#show page

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -3,10 +3,22 @@ require 'spec_helper'
 describe "User Pages" do
   subject { page }
 
+  User.create(name: "Sample User",
+              email: "sample.user@example.net",
+              password: "password",
+              password_confirmation: "password")
+  let(:user) { User.last }
+
   describe "signup page" do
     before { visit signup_path }
 
     it { should have_selector('h1', text: 'Sign up') }    
     it { should have_selector('title', text: full_title('Sign up')) }
+  end
+
+  describe "profile page" do
+    before { visit user_path(user) }
+
+    it { should have_selector('h1', text: 'Sample User') }
   end
 end
