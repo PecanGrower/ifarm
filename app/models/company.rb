175b221397344647334dddaefb1b36ada9a39bfa
Greg Daviet <Greg@theDaviets.com>
2013-02-23 00:46:19 -0700

    Add Company model

---
+++ app/models/company.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: companies
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Company < ActiveRecord::Base
+  attr_accessible :name
+
+  has_many :users
+
+  validates :name, presence: true,
+                   length: { maximum: 50 }
+end
---
+++ spec/models/company_spec.rb
@@ -0,0 +1,54 @@
+# == Schema Information
+#
+# Table name: companies
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+require 'spec_helper'
+
+describe Company do
+
+  valid_attributes = { name: "Pecan Farm" }
+
+  before { @company = Company.new(valid_attributes) }
+
+  subject { @company }
+
+  it { should be_valid }
+
+  describe "attributes" do
+    it { should respond_to :name }
+
+    context "protected from mass assignment" do
+      
+    end
+
+    context "from associations" do
+      it { should respond_to :users }
+    end
+  end
+
+  describe "validations" do
+    
+    context "for name" do
+
+      context "when blank" do
+        before { @company.name = " " }
+        it { should be_invalid }
+      end
+
+      context "when too long" do
+        before { @company.name = "a" * 51 }
+        it { should be_invalid }
+      end
+    end
+  end
+
+  describe "methods" do
+    
+  end
+end
