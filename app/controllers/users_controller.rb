7ff57a268e4c9dfd8f2dc8f64346797e0df81a8e
Greg Daviet <Greg@theDaviets.com>
2013-02-22 17:10:24 -0700

    Add User Signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,11 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
+  end
+
+  def create
+    # @user = User.create(params[:user])
+    # render user_path(@user)
   end
 end
