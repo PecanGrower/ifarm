0c9955f9e9342cc0af511470f6b6c85e1596335f
Greg Daviet <Greg@theDaviets.com>
2013-02-24 04:19:06 -0700

    Add 'signin(user)' helper

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,1 +1,13 @@
 include ApplicationHelper
+
+def sign_in(user)
+  visit signin_path
+  fill_in "Email", with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+  #Sign in when not using Capybara as well.
+  #session[:remember_token] = user.remember_token
+  #The above code doesn't work. 'session' is unavailable in integeration tests.
+  #If a non-Capybara login is needed, use:
+  #post signin_path, email: user.email, password: user.password
+end
