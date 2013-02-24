be0575b34de5401b2a99c8df07d579b11aed3cf0
Greg Daviet <Greg@theDaviets.com>
2013-02-24 13:06:07 -0700

    Refactor user_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -31,103 +31,57 @@ describe User do
   end
 
   describe "attributes" do
-    it { should respond_to(:email) }
-    it { should respond_to(:password_digest) }
+    it { should have_db_column(:email) }
+    it { should have_db_column(:password_digest) }
+    it { should have_db_column(:remember_token) }
+    it { should have_db_column(:company_id) }
     it { should respond_to(:password) }
     it { should respond_to(:password_confirmation) }
-    it { should respond_to(:remember_token) }
-    it { should respond_to(:company_id) }
 
     context "protected from mass assignment" do
       it { should_not allow_mass_assignment_of :password_digest }
       it { should_not allow_mass_assignment_of :remember_token }
       it { should_not allow_mass_assignment_of :company_id }
     end
+  end
 
-    context "from associations" do
-      it { should respond_to :company }
+  describe "associations" do
+    it { should respond_to :company }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email).case_insensitive }
+    it { should ensure_length_of(:password).is_at_least(6) }
+    it { should validate_confirmation_of(:password) }
+    it { should validate_presence_of(:password_confirmation) }
+
+    it "should validate format of e-mail" do
+      valid = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+      invalid = %w[user@foo,com user_at_foo.org example.user@foo.
+                             foo@bar_baz.com foo@bar+baz.com]
+      
+      valid.each do |valid_address|
+        expect(user).to validate_format_of(:email).with(valid_address)
+      end      
+      
+      invalid.each do |invalid_address|
+        expect(user).to validate_format_of(:email).not_with(invalid_address)
+      end      
     end
 
-    context "that validates" do
-      context "email" do
-        it { should validate_presence_of(:email) }
-        context "when email format is invalid" do
-          it "should be invalid" do
-            addresses = %w[user@foo,com user_at_foo.org example.user@foo.
-                           foo@bar_baz.com foo@bar+baz.com]
-            addresses.each do |invalid_address|
-              user.email = invalid_address
-              user.should_not be_valid
-            end      
-          end
-        end
-        context "when email format is valid" do
-          it "should be valid" do
-            addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
-            addresses.each do |valid_address|
-              user.email = valid_address
-              user.should be_valid
-            end      
-          end
-        end
-        context "when email address is already taken" do
-          before do
-            user_with_same_email = user.dup
-            user_with_same_email.email = user.email.upcase
-            user_with_same_email.save
-          end
-
-          it { should_not be_valid }
-        end
-        context "email address with mixed case" do
-          let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
-
-          it "should be saved as all lower-case" do
-            user.email = mixed_case_email
-            user.save
-            user.reload.email.should == mixed_case_email.downcase
-          end
-        end
-      end
-
-      context "password" do
-        it { should ensure_length_of(:password).is_at_least(6) }
-        context "when password is not present" do
-          before { user.password = user.password_confirmation = " " }
-          it { should_not be_valid }
-        end
-        context "when password doesn't match confirmation" do
-          before { user.password_confirmation = "mismatch" }
-          it { should_not be_valid }
-        end
-      end
-
-      it { should validate_presence_of(:password_confirmation) }
+    it "should be save email as all lower-case" do
+      mixed_case_email = "Foo@ExAMPle.CoM"
+      user.email = mixed_case_email
+      user.save
+      user.reload.email.should == mixed_case_email.downcase
     end
   end
 
   describe "methods" do
-      it { should respond_to(:authenticate) }
-
-      describe "return value of authenticate method" do
-        before { user.save }
-        let(:found_user) { User.find_by_email(user.email) }
-
-        context "with valid password" do
-          it { should == found_user.authenticate(user.password) }
-        end
-
-        context "with invalid password" do
-          let(:user_for_invalid_password) { found_user.authenticate("invalid") }
-
-          it { should_not == user_for_invalid_password }
-          specify { user_for_invalid_password.should be_false }
-        end
-      end
-  
-      describe "remember_token" do
-        before { user.save }
-        its(:remember_token) { should_not be_blank }
-      end
+    describe "create_remember_token" do
+      before { user.save }
+      its(:remember_token) { should_not be_blank }
+    end
   end
 end
