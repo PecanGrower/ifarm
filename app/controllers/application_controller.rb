264006387ce58d91180d6994b659bcc635f1f765
Greg Daviet <Greg@theDaviets.com>
2013-02-27 13:59:04 -0700

    Move controller methods from session_helper to application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  include SessionsHelper
 
   before_filter :signed_in_user
 
@@ -10,6 +9,43 @@ class ApplicationController < ActionController::Base
         store_location
         redirect_to signin_url, notice: 'Please sign in.' 
       end
+    end
+
+    def sign_in(user)
+      session[:remember_token] = user.remember_token
+      self.current_user = user
+    end
+
+    def signed_in?
+      !current_user.nil?
+    end
+    helper_method :signed_in?
+
+    def sign_out
+      self.current_user = nil
+      session.delete(:remember_token)
+    end
+
+    def current_user=(user)
+      @current_user = user
+    end
+
+    def current_user
+      @current_user ||= User.find_by_remember_token(session[:remember_token])
+    end
+    helper_method :current_user
+
+    def current_user?(user)
+      user == current_user
+    end
+
+    def store_location
+      session[:return_to] = request.url
+    end
+
+    def redirect_back_or(default)
+      redirect_to(session[:return_to] || default)
+      session.delete(:return_to)
     end
 
 
