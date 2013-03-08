e2a1b0f5e88c6cbfff3f81a2e88fcfb638513ef1
Greg Daviet <Greg@theDaviets.com>
2013-03-08 12:19:53 -0700

    Add Farm#new page

--- app/controllers/farms_controller.rb
+++ app/controllers/farms_controller.rb
@@ -4,4 +4,18 @@ class FarmsController < ApplicationController
     @farms = Farm.all
   end
 
+  def new
+    @farm = Farm.new
+  end
+
+  def create
+    @farm = Farm.new(params[:farm])
+    if @farm.save
+      flash[:success] = "New farm successfully added."
+      redirect_to farms_path
+    else
+      render 'new'
+    end
+  end
+
 end
--- spec/requests/farm_pages_spec.rb
+++ spec/requests/farm_pages_spec.rb
@@ -14,10 +14,31 @@ describe "Farm" do
   describe "index page" do
 
     let!(:farm) { FactoryGirl.create(:farm) }
+    let!(:block) { FactoryGirl.create(:block) }
+    let!(:field) { FactoryGirl.create(:field) }
 
     before { visit farms_path }
 
     it { should have_selector 'title', text: full_title('Farm') }
     it { should have_selector 'td', text: farm.name }
+    it { should have_selector 'td', text: block.name }
+    it { should have_selector 'td', text: field.name}
+    it { should have_link('New Farm', href: new_farm_path) }
+  end
+
+  describe "new page" do
+    before { visit new_farm_path }
+    let(:submit) { "Create New Farm" }
+
+    it { should have_selector 'title', text: full_title('Add Farm') }
+    it { should have_selector 'h1', text: 'Add Farm' }
+
+    it "should create a new farm" do
+      fill_in "Farm Name", with: "Farm #1"
+      expect do
+        click_button submit
+        Company.current_id = user.company.id
+      end.to change(Farm, :count).by(1)
+    end
   end
 end
