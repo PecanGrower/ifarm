e82725b4dbf0604a46812c9ddb19565baeb61f8c
Greg Daviet <Greg@theDaviets.com>
2013-03-21 04:34:29 -0600

    Add irrigation schedule formula to @irrigation.next_irrigation

--- app/models/irrigation.rb
+++ app/models/irrigation.rb
@@ -8,7 +8,15 @@ class Irrigation < ActiveRecord::Base
   validates :time, presence: true
 
   def next_irrigation
-    time + 10.days
+    d = time.yday
+    t5 = (d ** 5) *  0.00000000081405
+    t4 = (d ** 4) * -0.00000064165
+    t3 = (d ** 3) *  0.00018096
+    t2 = (d ** 2) * -0.019768
+    t1 = (d ** 1) * 0.23585
+    t0 = (d ** 0) * 72.258
+    interval = t5 + t4 + t3 + t2 + t1 + t0
+    time + interval.days
   end
 
 end
