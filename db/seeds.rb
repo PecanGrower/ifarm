bc96a4478dee8d1f1e9f62a195638137a73ba0d2
Greg Daviet <Greg@theDaviets.com>
2013-03-24 00:40:46 -0600

    Add seed data to db:test for Et and Kc

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Rake::Task["import:et"].invoke
+Rake::Task["import:kc"].invoke
--- lib/tasks/import.rake
+++ lib/tasks/import.rake
@@ -25,3 +25,11 @@ namespace :import do
     end
   end  
 end
+
+namespace :db do
+  namespace :test do
+    task prepare: :environment do
+      Rake::Task["db:seed"].invoke
+    end
+  end
+end
